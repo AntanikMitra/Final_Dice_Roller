cdf6cdebaafa5d37460f2305c64c66b71bea8208
antanikmitra <antanikmitra@hotmail.com>
2016-04-21 11:44:10 -0400

    Last commit

--- scripts/app.js
+++ scripts/app.js
@@ -19,34 +19,6 @@ function ranNumber() {
     // Injecting into html
     ranOne.innerHTML = String(dice1);
     ranTwo.innerHTML = String(dice2);
-    
-    // switch case
-    switch () {
-    case 0:
-       document.getElementById("diceOne").setAttribute("href", "../content/images/1.png"); 
-       document.getElementById("diceTwo").setAttribute("href", "../content/images/1.png"); 
-        break;
-    case 1:
-        document.getElementById("diceOne").setAttribute("href", "../content/images/2.png"); 
-        document.getElementById("diceTwo").setAttribute("href", "../content/images/2.png"); 
-        break;
-    case 2:
-        document.getElementById("diceOne").setAttribute("href", "../content/images/3.png");
-        document.getElementById("diceTwo").setAttribute("href", "../content/images/3.png");  
-        break;
-    case 3:
-        document.getElementById("diceOne").setAttribute("href", "../content/images/4.png"); 
-        document.getElementById("diceTwo").setAttribute("href", "../content/images/4.png"); 
-        break;
-    case 4:
-        document.getElementById("diceOne").setAttribute("href", "../content/images/5.png"); 
-        document.getElementById("diceTwo").setAttribute("href", "../content/images/5.png"); 
-        break;
-    case 5:
-        document.getElementById("diceOne").setAttribute("href", "../content/images/6.png");
-        document.getElementById("diceTwo").setAttribute("href", "../content/images/6.png");  
-        break;
-   
-}
+       
     
 }
